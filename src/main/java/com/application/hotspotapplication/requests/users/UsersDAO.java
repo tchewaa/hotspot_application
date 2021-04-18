18957ac593c24894f819a0febb0d9eb3ba28b4c8
bradleyp <bradleyp@bbd.co.za>
2021-04-18 12:57:16 +0200

    Added more requests to User Fixed API Response Exceptions  - Can be bettered

--- src/main/java/com/application/hotspotapplication/requests/users/UsersDAO.java
+++ src/main/java/com/application/hotspotapplication/requests/users/UsersDAO.java
@@ -1,12 +1,25 @@
 package com.application.hotspotapplication.requests.users;
 
+import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UsersDAO extends JpaRepository<Users, Long> {
 
+  @Modifying
+  @Transactional
+  @Query("UPDATE Users SET firstName = :firstName, lastName = :lastName, email = :email WHERE id = :id")
+  void updateUser(@Param("firstName") String firstName, @Param("lastName") String lastName, @Param("email") String email, @Param("id")Long id);
+
+  @Modifying
+  @Transactional
+  @Query("UPDATE Users SET active = :activate WHERE id = :id")
+  void activateUser(@Param("activate") boolean activate, @Param("id")Long id);
+
   Users findUsersById(Long id);
 
-  //@Query(value = "SELECT * FROM users WHERE email = ?1", nativeQuery = true)
-  Users findUsersByEmail(String email);
+  Users findUserByEmail(String email);
 
 }
